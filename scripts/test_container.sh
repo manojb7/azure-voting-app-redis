c174c3860d468fb2ec5805396ad318bbb92e45ca
manojb7 <50066322+manojb7@users.noreply.github.com>
2023-06-18 17:27:23 +0530

    Update test_container.sh

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -4,12 +4,12 @@ count=0
 started=false
 
 while true; do
-    ((count++))
+    count=$((count + 1))
     echo "[$STAGE_NAME] Starting container [Attempt: $count]"
 
     testStart=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000)
 
-    if [[ $testStart -eq 200 ]]; then
+    if [ "$testStart" -eq 200 ]; then
         started=true
         break
     else
